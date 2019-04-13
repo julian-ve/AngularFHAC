15b05a798f041c627b8f3a232545ef1c7a149086
Julian <vecera.julian@gmail.com>
2019-04-13 19:38:44 +0200

    add overlay for images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { NasaApiService } from './services/nasa-api.service';
+import {INasaImage} from './INasaImage';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,24 @@ import { NasaApiService } from './services/nasa-api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  constructor(private nasaApiService: NasaApiService) {
+  }
   title = 'NASA Cameras';
 
   // private nasaApiService: NasaApiService;
-  private pictures = [];
+  private images = [];
+  private currentImage: INasaImage;
 
-  constructor(private nasaApiService: NasaApiService) {
-    this.nasaApiService = nasaApiService;
+  showImage(img: INasaImage) {
+    console.log('Show image: ' + img.id);
+    this.currentImage = img;
+    console.log(img);
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  hideOverlay() {
+    document.getElementById('overlay').style.display = 'none';
   }
 
   ngOnInit() {
@@ -22,8 +34,10 @@ export class AppComponent implements OnInit {
 
   showPictures() {
     this.nasaApiService.getPicture()
-      .subscribe(data => { this.pictures = data; },
+      .subscribe(data => { this.images = data; },
         err => console.log(err),
         () => console.log('Loaded photos from api'));
   }
+
+
 }
